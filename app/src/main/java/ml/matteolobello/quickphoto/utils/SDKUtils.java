3ff595a210f6e5761af8addbb2cf9d233f65b2e9
Matteo Lobello <matteolob1704@gmail.com>
2017-05-25 19:24:39 +0200

    Fix a little typo

--- app/src/main/java/ml/matteolobello/quickphoto/utils/SDKUtils.java
+++ app/src/main/java/ml/matteolobello/quickphoto/utils/SDKUtils.java
@@ -10,7 +10,7 @@ public class SDKUtils {
     public static final boolean AT_LEAST_NOUGAT = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1;
 
     /**
-     * Check if we're running at least Android 6.0 Nougat
+     * Check if we're running at least Android 6.0 Marshmallow
      */
     public static final boolean AT_LEAST_MARSHMALLOW = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
 }
